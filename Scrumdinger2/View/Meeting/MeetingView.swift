a883694476bc98a6fc6dd554b693907c40d05d2d
oscar <oscarobrien@live.com>
2023-03-05 19:10:04 +0100

    meeting view and timer

---
+++ Scrumdinger2/View/Meeting/MeetingView.swift
@@ -0,0 +1,52 @@
+//
+//  MeetingView.swift
+//  Scrumdinger2
+//
+//  Created by Oscar O'Brien on 5/3/2023.
+//
+
+import SwiftUI
+import AVFoundation
+
+struct MeetingView: View {
+    
+    @Binding var scrum: DailyScrum
+    @StateObject var scrumTimer = ScrumTimer()
+    
+    private var player: AVPlayer { AVPlayer.sharedDingPlayer }
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 16.0)
+                .fill(scrum.theme.mainColor)
+            
+            VStack {
+                MeetingHeaderView(secondsElapsed: scrumTimer.secondsElapsed, secondsRemaining: scrumTimer.secondsRemaining, theme: scrum.theme)
+                Circle().strokeBorder(lineWidth: 24)
+                
+                MeetingFooterView(speakers: scrumTimer.speakers, skipAction: scrumTimer.skipSpeaker)
+            }
+        }
+        .padding()
+        .foregroundColor(scrum.theme.accentColor)
+        .navigationBarTitleDisplayMode(.inline)
+        .onAppear {
+            scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes, attendees: scrum.attendees)
+            scrumTimer.speakerChangedAction = {
+                player.seek(to: .zero)
+                player.play()
+            }
+            scrumTimer.startScrum()
+        }
+        .onDisappear {
+            scrumTimer.stopScrum()
+        }
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView(scrum: .constant(DailyScrum.sampleData[0]))
+    }
+}
+
