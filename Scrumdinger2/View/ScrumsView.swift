d6ddcf7b1714de4a0fa46c31e7cddd88e6c3896e
oscar <oscarobrien@live.com>
2023-03-05 21:20:18 +0100

    data persistence

--- Scrumdinger2/View/ScrumsView.swift
+++ Scrumdinger2/View/ScrumsView.swift
@@ -10,9 +10,10 @@ import SwiftUI
 struct ScrumsView: View {
     
     @Binding var scrums: [DailyScrum]
+    @Environment(\.scenePhase) private var scenePhase
     @State private var isPresentingNewScrumView = false
     @State private var newScrumData = DailyScrum.Data()
-    
+    let saveAction: () -> Void
     
     var body: some View {
         List {
@@ -52,6 +53,9 @@ struct ScrumsView: View {
                         }
                     }
             }
+            .onChange(of: scenePhase) { phase in
+                if phase == .inactive { saveAction() }
+            }
         }
     }
 }
@@ -60,7 +64,7 @@ struct SvcrumsView_Previews: PreviewProvider {
     
     static var previews: some View {
         NavigationView {
-            ScrumsView(scrums: .constant(DailyScrum.sampleData))
+            ScrumsView(scrums: .constant(DailyScrum.sampleData), saveAction: {})
         }
     }
 }
