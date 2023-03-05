82e91c21a4c42cdc62dc743d00010cf4e409792a
oscar <oscarobrien@live.com>
2023-03-05 14:24:30 +0100

    Initial Commit

---
+++ Scrumdinger2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Scrumdinger2
+//
+//  Created by Oscar O'Brien on 5/3/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Scrumdinger2/Scrumdinger2App.swift
@@ -0,0 +1,17 @@
+//
+//  Scrumdinger2App.swift
+//  Scrumdinger2
+//
+//  Created by Oscar O'Brien on 5/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct Scrumdinger2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
