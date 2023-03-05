6da6dff06d0725c5efbf8d0c07cf86fa2c6c54d5
oscar <oscarobrien@live.com>
2023-03-05 16:36:37 +0100

    created edit view

--- Scrumdinger2/View/DetailsView.swift
+++ Scrumdinger2/View/DetailsView.swift
@@ -11,6 +11,8 @@ struct DetailsView: View {
     
     let scrum: DailyScrum
     
+    @State private var isPresentingEditView = false
+    
     var body: some View {
         List {
             Section(header: Text("Meeting Info")) {
@@ -42,6 +44,29 @@ struct DetailsView: View {
             }
         }
         .navigationTitle(scrum.title)
+        .toolbar {
+            Button("Edit") {
+                isPresentingEditView = true
+            }
+        }
+        .sheet(isPresented: $isPresentingEditView) {
+            NavigationView {
+                DetailEditView()
+                    .navigationTitle(scrum.title)
+                    .toolbar {
+                        ToolbarItem(placement: .cancellationAction) {
+                            Button("Cancel") {
+                                isPresentingEditView = false
+                            }
+                        }
+                        ToolbarItem(placement: .confirmationAction) {
+                            Button("Done") {
+                                isPresentingEditView = false
+                            }
+                        }
+                }
+            }
+        }
     }
 }
 
@@ -52,4 +77,3 @@ struct DetailsView_Previews: PreviewProvider {
         }
     }
 }
-
